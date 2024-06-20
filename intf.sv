ab858f5df8eca6a8ba9d8bc43fd6504ba29a6e64
ahmedosama7450 <a.osama7450@gmail.com>
2024-06-20 22:36:16 +0300

    reformat

--- intf.sv
+++ intf.sv
@@ -2,8 +2,8 @@ interface intf ();
   logic clk;
   logic rst_n;
   logic fetch_enable;
-  logic program_finished;
 
+  logic program_finished;
   logic [127:0] data;
   // logic [127:0] key;
   logic [127:0] result_enc;
@@ -13,8 +13,8 @@ interface intf ();
       input clk,
       input rst_n,
       input fetch_enable,
-      output program_finished,
 
+      output program_finished,
       output data,
       output result_enc,
       output result_dec
