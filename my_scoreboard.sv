10a186beea6d0aeb816a860a7a1ca1d3e0109d72
ahmedosama7450 <a.osama7450@gmail.com>
2024-06-20 22:37:12 +0300

    Add scoreboard

--- my_scoreboard.sv
+++ my_scoreboard.sv
@@ -5,6 +5,13 @@ class my_scoreboard extends uvm_scoreboard;
 
   uvm_analysis_imp #(my_sequence_item, my_scoreboard) aimp;
 
+  int enc_pass_count, enc_fail_count, enc_no_output_count;
+  int dec_pass_count, dec_fail_count;
+  int total_transactions_count;
+
+  logic [127:0] golden_model_out;
+  int outputFile;
+
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
@@ -19,11 +26,55 @@ class my_scoreboard extends uvm_scoreboard;
 
   function void report_phase(uvm_phase phase);
     super.report_phase(phase);
-    // TODO
+
+    `uvm_info(get_full_name(), $sformatf("\nTotal transactions count: %0d", total_transactions_count),
+              UVM_LOW)
+
+    `uvm_info(get_full_name(), $sformatf(
+              "\n---Encryption---\nPass count: %0d\nFail count: %0d\nNo output count: %0d\n",
+              enc_pass_count,
+              enc_fail_count,
+              enc_no_output_count
+              ), UVM_LOW)
+
+    `uvm_info(
+        get_full_name(), $sformatf(
+        "\n---Decryption---\nPass count: %0d\nFail count: %0d\n", dec_pass_count, dec_fail_count,),
+        UVM_LOW)
   endfunction
 
-  function void write(my_sequence_item my_sequence_item_h);
-    // TODO
+  function void write(my_sequence_item t);
+    total_transactions_count++;
+
+    // Decryption
+    if (t.data == t.result_dec) begin
+      $display("TX %0d, Decryption passed: Original data: %h, Processor output: %h",
+               total_transactions_count, t.data, t.result_dec);
+      dec_pass_count++;
+    end else begin
+      $display("TX %0d, Decryption failed: Original data: %h, Processor output: %h",
+               total_transactions_count, t.data, t.result_dec);
+      dec_fail_count++;
+    end
+
+    // Encryption
+    outputFile = $fopen("output.txt", "r");
+
+    if ($fscanf(outputFile, "%h", golden_model_out) != 1) begin
+      $display("TX %0d, No encryption output", total_transactions_count);
+      enc_no_output_count++;
+    end else if (golden_model_out == t.result_enc) begin
+      $display("TX %0d, Encryption passed: Golden model output: %h, Processor output: %h",
+               total_transactions_count, golden_model_out, t.result_enc);
+      enc_pass_count++;
+    end else begin
+      $display("TX %0d, Encryption failed: Golden model output: %h, Processor output: %h",
+               total_transactions_count, golden_model_out, t.result_enc);
+      enc_fail_count++;
+    end
+
+    $fclose(outputFile);
+
   endfunction
 
 endclass
--- my_sequence.sv
+++ my_sequence.sv
@@ -1,13 +1,15 @@
 class my_sequence extends uvm_sequence;
   `uvm_object_utils(my_sequence)
 
+  int inputsFile;
+
   function new(string name = "my_sequence");
     super.new(name);
   endfunction
 
   task body();
 
-    for (int i = 0; i < 5; i++) begin
+    for (int i = 0; i < 100; i++) begin
 
       my_sequence_item my_sequence_item_h;
       my_sequence_item_h = my_sequence_item::type_id::create("my_sequence_item_h");
@@ -16,8 +18,12 @@ class my_sequence extends uvm_sequence;
 
       if (!my_sequence_item_h.randomize()) `uvm_fatal(get_full_name(), "randomize failed")
 
+      inputsFile = $fopen("inputs.txt", "w");
+      $fwrite(inputsFile, "%h\n%h", my_sequence_item_h.data, my_sequence_item_h.key);
+      $fclose(inputsFile);
+      $system("python D:/College/GP/aes-coprocessor-uvm/aes_model.py");
+
       finish_item(my_sequence_item_h);
-      
     end
 
   endtask
